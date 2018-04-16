49896d094c668afbfc62d3773e0088f137dc6c0b
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-04-16 11:57:51 -0400

    start components, install router

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <img src="logo.png" alt="logo" />
+        <h1>RentCheck</h1>
+        <nav />
+      </header>
+    )
+  }
+}
+
+export default Header
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Intro extends Component {
+  render() {
+    return <div className="intro">hello</div>
+  }
+}
+
+export default Intro
