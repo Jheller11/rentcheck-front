70bf6473d88e6b1c1731dd1be5b60e1ae7b5bf8a
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-04-19 16:11:33 -0400

    style app

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -6,29 +6,38 @@ class Intro extends Component {
     return (
       <div>
         <div className="intro">
-          <div className="column">
-            <h3>For Renters:</h3>
-            <p>
-              Use this tool to get a market-based rent estimate for your next
-              Washington, DC apartment. Click{' '}
-              <Link to="/apartments/new">here</Link> to get started!
-            </p>
+          <div className="flex">
+            <div>
+              <h3>For Renters:</h3>
+              <p>
+                Use this tool to get a market-based rent estimate for your next
+                Washington, DC apartment. Click{' '}
+                <Link to="/apartments/new">here</Link> to get started!
+              </p>
+            </div>
+            <img src="https://images.pexels.com/photos/161768/lafayette-park-washington-dc-c-architecture-161768.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
           </div>
-          <div className="column">
-            <h3>For Landlords:</h3>
-            <p>
-              Renting out your basement or condo? Use the tool to get the best
-              value for your property based on similar rentals in your area.
-              Click <Link to="/apartments/new">here</Link> to get started!
-            </p>
+          <div className="flex">
+            <img src="https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+            <div>
+              <h3>For Landlords:</h3>
+              <p>
+                Renting out your basement or condo? Use the tool to get the best
+                value for your property based on similar rentals in your area.
+                Click <Link to="/apartments/new">here</Link> to get started!
+              </p>
+            </div>
           </div>
         </div>
-        <div className="content">
-          <h3>Questions?</h3>
-          <p>
-            Check out the <Link to="/questions/">F.A.Q.</Link> for more
-            information!
-          </p>
+        <div className="flex">
+          <div>
+            <h3>Questions?</h3>
+            <p>
+              Check out the <Link to="/questions/">F.A.Q.</Link> for more
+              information!
+            </p>
+          </div>
+          <img src="https://images.pexels.com/photos/129112/pexels-photo-129112.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
         </div>
       </div>
     )
