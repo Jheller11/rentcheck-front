ee094079f9948f42fbb1f333f2b62cb92cdaac93
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-04-20 12:44:55 -0400

    redeploy

--- src/components/EstimateForm/EstimateForm.js
+++ src/components/EstimateForm/EstimateForm.js
@@ -47,87 +47,95 @@ class EstimateForm extends Component {
   componentDidMount() {}
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <h3>Fill out this form to receive your estimate!</h3>
-          <div className="form-row">
-            <label htmlFor="apartment">I am a:</label>
-            <select name="type" onChange={this.handleInput}>
-              <option> </option>
-              <option value="renter">Renter</option>
-              <option value="landlord">Landlord</option>
-            </select>
-          </div>
-          <div className="form-row">
-            <label htmlFor="apartment">Bedrooms:</label>
-            <select name="bedrooms" onChange={this.handleInput}>
-              <option> </option>
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-          </div>
-          <div className="form-row">
-            <label htmlFor="apartment">Square Feet:</label>
-            <input type="number" name="size" onChange={this.handleInput} />
-          </div>
-          <div className="form-row">
-            <label htmlFor="apartment">Bathrooms:</label>
-            <select name="baths" onChange={this.handleInput}>
-              <option> </option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-          </div>
-          <div className="form-row">
-            <label htmlFor="apartment">Parking Spots:</label>
-            <select name="parking" onChange={this.handleInput}>
-              <option> </option>
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-            </select>
-          </div>
-          <div className="form-row">
-            <label htmlFor="apartment">Neighborhood:</label>
-            <select name="neighborhood" onChange={this.handleInput}>
-              <option> </option>
-              <option value="5">Dupont Circle</option>
-              <option value="5">Georgetown</option>
-              <option value="5">Logan Circle</option>
-              <option value="5">Shaw</option>
-              <option value="5">Mt. Vernon</option>
-              <option value="4">Downtown</option>
-              <option value="4">Foggy Bottom</option>
-              <option value="4">AU/Tenleytown</option>
-              <option value="4">U St.</option>
-              <option value="4">Adams Morgan</option>
-              <option value="3">Friendship Heights</option>
-              <option value="3">Chevy Chase</option>
-              <option value="3">Cleveland Park</option>
-              <option value="3">Woodley Park</option>
-              <option value="3">Glover Park</option>
-              <option value="3">Southwest</option>
-              <option value="3">Capitol Hill</option>
-              <option value="3">Mt. Pleasant</option>
-              <option value="3">Columbia Heights</option>
-              <option value="3">Petworth</option>
-              <option value="2">Brookland</option>
-              <option value="2">Brightwood</option>
-              <option value="2">Ft. Totten</option>
-              <option value="2">Stadium-Armory</option>
-              <option value="2">Brentwood</option>
-              <option value="2">Ledroit Park</option>
-              <option value="2">Takoma</option>
-              <option value="1">Anacostia</option>
-              <option value="1">Deanwood</option>
-              <option value="3">Other</option>
-            </select>
-          </div>
-          <input type="submit" />
-        </form>
+      <div className="flex">
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <h3>Fill out this form to receive your estimate!</h3>
+            <div className="form-row">
+              <label htmlFor="apartment">I am a:</label>
+              <select name="type" onChange={this.handleInput}>
+                <option> </option>
+                <option value="renter">Renter</option>
+                <option value="landlord">Landlord</option>
+              </select>
+            </div>
+            <div className="form-row">
+              <label htmlFor="apartment">Bedrooms:</label>
+              <select name="bedrooms" onChange={this.handleInput}>
+                <option> </option>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+            </div>
+            <div className="form-row">
+              <label htmlFor="apartment">Square Feet:</label>
+              <input type="number" name="size" onChange={this.handleInput} />
+            </div>
+            <div className="form-row">
+              <label htmlFor="apartment">Bathrooms:</label>
+              <select name="baths" onChange={this.handleInput}>
+                <option> </option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+            </div>
+            <div className="form-row">
+              <label htmlFor="apartment">Parking Spots:</label>
+              <select name="parking" onChange={this.handleInput}>
+                <option> </option>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+              </select>
+            </div>
+            <div className="form-row">
+              <label htmlFor="apartment">Neighborhood:</label>
+              <select name="neighborhood" onChange={this.handleInput}>
+                <option> </option>
+                <option value="5">Dupont Circle</option>
+                <option value="5">Georgetown</option>
+                <option value="5">Logan Circle</option>
+                <option value="5">Shaw</option>
+                <option value="5">Mt. Vernon</option>
+                <option value="4">Downtown</option>
+                <option value="4">Foggy Bottom</option>
+                <option value="4">AU/Tenleytown</option>
+                <option value="4">U St.</option>
+                <option value="4">Adams Morgan</option>
+                <option value="3">Friendship Heights</option>
+                <option value="3">Chevy Chase</option>
+                <option value="3">Cleveland Park</option>
+                <option value="3">Woodley Park</option>
+                <option value="3">Glover Park</option>
+                <option value="3">Southwest</option>
+                <option value="3">Capitol Hill</option>
+                <option value="3">Mt. Pleasant</option>
+                <option value="3">Columbia Heights</option>
+                <option value="3">Petworth</option>
+                <option value="2">Brookland</option>
+                <option value="2">Brightwood</option>
+                <option value="2">Ft. Totten</option>
+                <option value="2">Stadium-Armory</option>
+                <option value="2">Brentwood</option>
+                <option value="2">Ledroit Park</option>
+                <option value="2">Takoma</option>
+                <option value="1">Anacostia</option>
+                <option value="1">Deanwood</option>
+                <option value="3">Other</option>
+              </select>
+            </div>
+            <input type="submit" />
+          </form>
+        </div>
+        <div>
+          <img
+            className="calc"
+            src="https://images.pexels.com/photos/67599/calculator-solar-calculator-count-how-to-calculate-67599.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          />
+        </div>
       </div>
     )
   }
